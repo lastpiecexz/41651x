4d4ae910b9d5b7e5eb20382e260887e0a73472f2
lastpiecexu <notrealxu@gmail.com>
2018-05-11 14:56:24 +0800

    5.11

--- BaiduFanyiDemo/app/src/main/java/com/itfollowme/baidufanyidemo/MainActivity.java
+++ BaiduFanyiDemo/app/src/main/java/com/itfollowme/baidufanyidemo/MainActivity.java
@@ -7,11 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 import retrofit2.Retrofit;
@@ -40,20 +36,8 @@ public class MainActivity extends AppCompatActivity {
     String salt = String.valueOf(System.currentTimeMillis());
     String securityKey = "替换成你的";
     String sign = appid+q+salt+securityKey;
-    try {
-      MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-      byte[] inputArray = sign.getBytes("UTF-8");
-      messageDigest.update(inputArray);
-      // 转换并返回结果，也是字节数组，包含16个元素
-      byte[] resultByteArray = messageDigest.digest();
-      // 字符数组转换成字符串返回
-      sign =  byteArrayToHex(resultByteArray);
 
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    }
+    sign = Utils.MD5(sign);
 
     Map<String, String> params = new HashMap<>();
     params.put("q",q);
@@ -73,29 +57,11 @@ public class MainActivity extends AppCompatActivity {
     Flowable<Translate> flowable = fanyiApi.fanyi(params);
     flowable.subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Consumer<Translate>() {
-          @Override
-          public void accept(Translate translate) throws Exception {
+        .subscribe(translate ->{
             mTvTarget.setText(translate.toString());
-          }
         });
 
   }
 
-  private static String byteArrayToHex(byte[] byteArray) {
-    // new一个字符数组，这个就是用来组成结果字符串的（解释一下：一个byte是八位二进制，也就是2位十六进制字符（2的8次方等于16的2次方））
-    char[] resultCharArray = new char[byteArray.length * 2];
-    // 遍历字节数组，通过位运算（位运算效率高），转换成字符放到字符数组中去
-    int index = 0;
-    for (byte b : byteArray) {
-      resultCharArray[index++] = hexDigits[b >>> 4 & 0xf];
-      resultCharArray[index++] = hexDigits[b & 0xf];
-    }
 
-    // 字符数组组合成字符串返回
-    return new String(resultCharArray);
-
-  }
-  private static final char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
-      'e', 'f' };
 }
---
+++ GliderDemo/app/src/main/java/com/itfollowme/gliderdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.itfollowme.gliderdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+public class MainActivity extends AppCompatActivity {
+
+  private ImageView mIvMeinv;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mIvMeinv = findViewById(R.id.iv_meinv);
+    String bigImgUrl = "https://desk-fd.zol-img.com.cn/t_s2880x1800c5/g5/M00/0E/01/ChMkJlnJ5u6ICodHAMbEg1lM-tEAAgzIgK3oP4AxsSb066.jpg";
+    String thumbUrl = "http://7xi8d6.com1.z0.glb.clouddn.com/20180129074038_O3ydq4_Screenshot.jpeg";
+    //String url = "http://storage.slide.news.sina.com.cn/slidenews/77_ori/2018_19/74766_822277_623320.gif";
+    GlideApp.with(this)
+        .load(bigImgUrl)
+        .thumbnail(0.1f)
+        .into(mIvMeinv);
+  }
+}
